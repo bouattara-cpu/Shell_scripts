6cc2863a109de22f30d3bcbe6d177ce72fd43098
Billy Mellika Assita Ouattara <b.ouattara@alustudent.com>
2026-05-26 10:32:06 +0200

    If and else in script

---
+++ assignment_dir/verify.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+Directory_Name="assignment_dir"
+path="/c/Users/user/Shell_scripts/assignment_dir"
+
+if [ -d "$Path" ];
+then
+	echo "Directory with a Name $Directory_Name exists in the Location"
+else
+	echo "No such a Directory"
+fi
